5c4de02075e39caf374f4abb6d651c61777a57d5
rakesh.ardanuy <rakesh.gangwar@ardanuy.com>
2023-03-24 09:30:33 +0530

    update

--- SalesApp/WebAPI.Data/ProductData.cs
+++ SalesApp/WebAPI.Data/ProductData.cs
@@ -33,185 +33,10 @@ namespace SalesApp.WebAPI.Data
         {
             configuration = _configuration;
             this._hostEnvironment = hostEnvironment;
-            //this.NoImage = Path.Combine(hostEnvironment.WebRootPath + "/images/", "no-image.png");
-
-            //var abs = CommonHelper.AbsolutePath("/images/no-image.png");
-
-
             var request = httpContextAccessor.HttpContext.Request;
-            //var domain = $"{request.Scheme}://{request.Host}";
-
-
-
             this.BaseUrl = $"{request.Scheme}://{configuration.GetConnectionString("ImageHost").ToString()}";
             this.NoImage = Path.Combine(this.BaseUrl, "no-image.png");
-
-
         }
-
-
-
-
-        //public async Task<ServiceResponse<UserModel>> LogInUser(LoginModel model)
-        //{
-        //    ServiceResponse<UserModel> obj = new ServiceResponse<UserModel>();
-        //    try
-        //    {
-        //        using (IDbConnection connection = new SqlConnection(configuration.GetConnectionString("DBConnectionString").ToString()))
-        //        {
-        //            string sqlQuery = "select * from tblUser where UserName=@UserName and UserPassword=@UserPassword and IsActive=@IsActive";
-
-        //            var result = (await connection.QueryAsync(sqlQuery,
-        //                new
-        //                {
-        //                    @UserName = model.UserName,
-        //                    @UserPassword = model.Password,
-        //                    @IsActive = model.IsActive
-        //                })).FirstOrDefault();
-
-        //            if (result.UserId > 0)
-        //            {
-
-        //                var resObj = new UserModel
-        //                {
-        //                    UserId = result.UserId,
-
-        //                    UserName = result.UserName,
-        //                };
-
-        //                string sqlstr = "insert Into tblLogin(UserId,LogIn) Values(@UserId,@LogIn) select SCOPE_IDENTITY();";
-        //                resObj.LoginInId = (long)(connection.Query<long>(sqlstr, new { @UserId = result.UserId, @LogIn = DateTime.Now }).First());
-
-        //                obj.Data = resObj;
-        //                obj.Result = obj.Data.LoginInId > 0 ? true : false;
-        //                obj.Message = obj.Data.LoginInId > 0 ? "Data Found." : "No Data found.";
-
-        //            }
-        //        };
-
-        //        return obj;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        obj.Message = ex.Message;
-        //        return obj;
-        //    }
-        //}
-
-        //public async Task<ServiceResponse<string>> LogOutUser(int userId)
-        //{
-        //    ServiceResponse<string> obj = new ServiceResponse<string>();
-        //    try
-        //    {
-        //        using (IDbConnection cnn = new SqlConnection(configuration.GetConnectionString("DBConnectionString").ToString()))
-        //        {
-        //            string sqlQuery = "Update tblLogin Set LogOut=@LogOut Where UserId=@UserId; ";
-
-        //            int rowsAffected = cnn.Execute(sqlQuery, new { @LogOut = DateTime.Now, @UserId = userId });
-        //            if (rowsAffected > 0)
-        //            {
-        //                obj.Result = true;
-        //                obj.Data = "Sucessfully  Created.";
-        //            }
-        //            else
-        //            {
-        //                obj.Data = null;
-        //                obj.Message = "Failed new creation.";
-        //            }
-
-        //        }
-
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        obj.Message = ex.Message;
-        //        return obj;
-        //    }
-        //    finally
-        //    {
-
-        //    }
-        //    return obj;
-        //}
-
-        //public async Task<ServiceResponse<string>> AddUser(AccountUserModel _model)
-        //{
-        //    ServiceResponse<string> sres = new ServiceResponse<string>();
-        //    IDbTransaction transaction = null;
-
-        //    try
-        //    {
-        //        using (IDbConnection cnn = new SqlConnection(configuration.GetConnectionString("DBConnectionString").ToString()))
-        //        {
-        //            if (cnn.State != ConnectionState.Open)
-        //                cnn.Open();
-        //            transaction = cnn.BeginTransaction();
-
-        //            string _query = "INSERT INTO tblUser (UserKey,UserType,UserName,UserPassword,Organization,Title,SSN,FirstName,MiddleName,LastName,DOB,Email,CellPhone,HomePhone,EmgPhone,EmgContact,Gender,MaritalStatus,Ethnicity,SupervisorId,IsActive,CreatedOn,CreatedBy) VALUES (@UserKey,@UserType,@UserName,@UserPassword,@Organization,@Title,@SSN,@FirstName,@MiddleName,@LastName,@DOB,@Email,@CellPhone,@HomePhone,@EmgPhone,@EmgContact,@Gender,@MaritalStatus,@Ethnicity,@SupervisorId,@IsActive,@CreatedOn,@CreatedBy); select SCOPE_IDENTITY();";
-
-        //            _model.UserId = (int)(cnn.ExecuteScalar<int>(_query, _model, transaction));
-        //            string sqlQuery = "Insert Into tblAddress (UserId,AddressType,FlatNo,Address,City,Country,State,ZipCode,Longitude,Latitude,CreatedOn,CreatedBy) Values (@UserId,@AddressType,@FlatNo,@Address,@City,@Country,@State,@ZipCode,@Longitude,@Latitude,@CreatedOn,@CreatedBy);";
-        //            int rowsAffected = cnn.Execute(sqlQuery, new
-        //            {
-        //                @UserId = _model.UserId,
-        //                @AddressType = 1,
-        //                @FlatNo = _model.HomeAddress.FlatNo,
-        //                @Address = _model.HomeAddress.Address,
-        //                @City = _model.HomeAddress.City,
-        //                @Country = "USA",
-        //                @State = _model.HomeAddress.State,
-        //                @ZipCode = _model.HomeAddress.ZipCode,
-        //                @Longitude = _model.HomeAddress.Longitude,
-        //                @Latitude = _model.HomeAddress.Latitude,
-        //                @CreatedOn = _model.CreatedOn,
-        //                @CreatedBy = _model.CreatedBy
-        //            }, transaction);
-        //            transaction.Commit();
-        //            if (rowsAffected > 0)
-        //            {
-        //                sres.Result = true;
-        //                sres.Data = "Sucessfully  Created.";
-        //            }
-        //            else
-        //            {
-        //                sres.Data = null;
-        //                sres.Message = "Failed new creation.";
-        //            }
-
-        //        }
-
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        if (transaction != null)
-        //        {
-        //            transaction.Rollback();
-        //        }
-        //        sres.Message = ex.Message;
-        //        return sres;
-        //    }
-        //    finally
-        //    {
-        //        if (transaction != null)
-        //            transaction.Dispose();
-        //    }
-        //    return sres;
-        //}
-
-        //public async Task<ServiceResponse<IEnumerable<AccountUserModel>>> GetUser(int userType)
-        //{
-        //    ServiceResponse<IEnumerable<AccountUserModel>> obj = new ServiceResponse<IEnumerable<AccountUserModel>>();
-        //    using (var connection = new SqlConnection(configuration.GetConnectionString("DBConnectionString").ToString()))
-        //    {
-        //        string sql = "Select * from tblUser Where UserType=@UserType; ";
-        //        IEnumerable<AccountUserModel> results = (await connection.QueryAsync<AccountUserModel>(sql,
-        //                 new { @UserType = userType }));
-        //        obj.Data = results;
-        //        obj.Result = results.Any() ? true : false;
-        //        obj.Message = results.Any() ? "Data Found." : "No Data found.";
-        //    }
-        //    return obj;
-        //}
 
 
         public async Task<ServiceResponse<IEnumerable<ProductModel>>> GetProductList(int StoreId)
@@ -291,8 +116,10 @@ Where IM.MasterCompanyId=@StoreId and stock.CurrentProStatus=1 and stock.Pack=0;
                                    Description = itmGroup.FirstOrDefault().Description,
                                    UnitTypeId = itmGroup.FirstOrDefault().UnitTypeId != null ? itmGroup.FirstOrDefault().UnitTypeId : 0,
                                    UnitType = itmGroup.FirstOrDefault().UnitType,
-                                   PrimePhoto = itmGroup.FirstOrDefault().ImagePath != null ? Path.Combine(this.BaseUrl, (string)itmGroup.FirstOrDefault().ImagePath) : this.NoImage,
-                                   ProductImages = itmGroup.Where(x => x.ImagePath != null).Select(x => (Path.Combine(this.BaseUrl, (string)x.ImagePath))).ToList(),
+                                   
+                                   PrimePhoto = !string.IsNullOrEmpty(itmGroup.FirstOrDefault().ImagePath) ? Path.Combine(this.BaseUrl, (string)itmGroup.FirstOrDefault().ImagePath) : this.NoImage,
+                                   ProductImages = itmGroup.Where(x => !string.IsNullOrEmpty(x.ImagePath)).Select(x => (Path.Combine(this.BaseUrl, (string)x.ImagePath))).ToList(),
+                                  
                                    Price = itmGroup.FirstOrDefault().Price != null ? itmGroup.FirstOrDefault().Price : 0,
                                    Stocks = itmGroup.Select(x => (long)x.StockNo).ToList(),
                                    StockNos = itmGroup.Select(x => (string)x.TStockNo).ToList(),
@@ -373,8 +200,10 @@ Where IPM.ITEM_FINISHED_ID=@ItemFinishId;";
                                    Description = itmGroup.FirstOrDefault().Description,
                                    UnitTypeId = itmGroup.FirstOrDefault().UnitTypeId != null ? itmGroup.FirstOrDefault().UnitTypeId : 0,
                                    UnitType = itmGroup.FirstOrDefault().UnitType,
-                                   PrimePhoto = itmGroup.FirstOrDefault().ImagePath != null ? Path.Combine(this.BaseUrl, (string)itmGroup.FirstOrDefault().ImagePath) : this.NoImage,
-                                   ProductImages = itmGroup.Where(x => x.ImagePath != null).Select(x => (Path.Combine(this.BaseUrl, (string)x.ImagePath))).ToList(),
+
+                                   PrimePhoto = !string.IsNullOrEmpty(itmGroup.FirstOrDefault().ImagePath) ? Path.Combine(this.BaseUrl, (string)itmGroup.FirstOrDefault().ImagePath) : this.NoImage,
+                                   ProductImages = itmGroup.Where(x => !string.IsNullOrEmpty(x.ImagePath)).Select(x => (Path.Combine(this.BaseUrl, (string)x.ImagePath))).ToList(),
+
                                    Price = itmGroup.FirstOrDefault().Price != null ? itmGroup.FirstOrDefault().Price : 0,
                                    Stocks = itmGroup.Select(x => (long)x.StockNo).ToList(),
                                    StockNos = itmGroup.Select(x => (string)x.TStockNo).ToList(),
@@ -387,13 +216,6 @@ Where IPM.ITEM_FINISHED_ID=@ItemFinishId;";
             }
             return obj;
         }
-
-
-
-
-
-
-
 
 
 
